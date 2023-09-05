6c08d0bfdaf0e25cadef01a8cb59c1cccef0592e
root <root@concentrix.com>
2023-09-05 10:06:36 +0000

    email masking

--- bigbluebutton-html5/imports/utils/cnxAvalonUtils.js
+++ bigbluebutton-html5/imports/utils/cnxAvalonUtils.js
@@ -1,4 +1,14 @@
 import cnxCCValidation from "./cnxCCValidation";
+
+const validateEmail = (maskedText) => {
+    var validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+      emailMaskPattern="*****@*****.***";
+      if (maskedText.match(validRegex)) {
+              maskedText=emailMaskPattern;
+      }else{
+      }
+   return maskedText;
+}
 const avalonMask = (parsedMessage) => {
 	var maskedtext=parsedMessage;
   	//CNX-Avalon number masking
@@ -8,8 +18,11 @@ const avalonMask = (parsedMessage) => {
   	//maskedtext = maskedtext.replace(/apple/gi, 'XXXXX');
   	//maskedtext = maskedtext.replace(/avalon/gi, 'XXXXX');
   	//maskedtext = maskedtext.replace(/([a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9_-]+)/g, "*****@*****.***");
+	/*
 	maskedtext = maskedtext.replace(/([a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9_-]+\.[a-zA-Z0-9_-]+)/g, "*****@*****.**.**");
         maskedtext = maskedtext.replace(/([a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9_-]+[a-zA-Z0-9_-]+[a-zA-Z0-9_-]+)/g, "*****@*****.***");
+	*/
+	maskedtext=validateEmail(maskedtext);
 	return maskedtext;
 }
 const avalonMasking = (parsedMessage,eventType) => {
@@ -25,19 +38,26 @@ const avalonMasking = (parsedMessage,eventType) => {
        maskedtext = maskedtext.replace(/([a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9_-]+[a-zA-Z0-9_-]+[a-zA-Z0-9_-]+[^a-zA-Z0-9_-]$)/g, "*****@*****.***");
 
     }else{
+    /*	    
       maskedtext = maskedtext.replace(/([a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9_-]+\.[a-zA-Z0-9_-]+)/g, "*****@*****.**.**");
       maskedtext = maskedtext.replace(/([a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9_-]+[a-zA-Z0-9_-]+[a-zA-Z0-9_-]+)/g, "*****@*****.***");
+      */
+     maskedtext=validateEmail(maskedtext);
    }
   return maskedtext;
  }
+
 const handleMasking = (eventType,validatedQuestion) => {
     let maskedText='';
     if(validatedQuestion && validatedQuestion !== ''){
       let textTobeMasked=validatedQuestion;
       if(eventType !=='change'){
         maskedText=avalonMasking(textTobeMasked,eventType);
+      /*	      
         maskedText = maskedText.replace(/([a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9_-]+\.[a-zA-Z0-9_-]+)/g, "*****@*****.**.**");
         maskedText = maskedText.replace(/([a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9_-]+)/g, "*****@*****.***");
+      */
+      maskedText=validateEmail(maskedText);
       }else{
         const lastChar = textTobeMasked.substring(textTobeMasked.length - 1, textTobeMasked.length);
         const isNumeric = !isNaN(lastChar);
